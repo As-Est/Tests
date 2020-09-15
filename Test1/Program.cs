8b2ae06e3fb0c06e613f6060b7f0e38d4808e250
Иван Егоров <iwannadie004@gmail.com>
2020-09-15 20:49:35 +0700

    Добавьте файлы проекта.

---
+++ Test1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("input: ");
+            string s = Console.ReadLine();
+
+            int[] numbers = new int[10];
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                {
+                    numbers[int.Parse(s[i].ToString())]++;
+                }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (numbers[i] != 0)
+                {
+                    Console.WriteLine($"{i}: {numbers[i]}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Test2
+{
+    class Cycler
+    {
+        object[] list;
+        public Cycler(object[] list)
+        {
+            this.list = list;
+        }
+        int i = 0;
+        public object GetNext()
+        {
+            object item;
+            if (i >= list.Length)
+            {
+                i = 0;
+            }
+            item = list[i];
+            i++;
+            return item;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] list = { 1, 2, 3, 4, 5 };
+            Cycler cl = new Cycler(list);
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write(cl.GetNext() + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
